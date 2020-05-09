48fe09144033ad470ced551832be5f926bb1a792
chlagou ahmed <chlagou.ahmed@gmail.com>
2020-05-09 17:22:58 +0100

    add action direct

--- Assets/Scripts/Action Framework/Action Spawners/Action Direct/ActionDirectSystem.cs
+++ Assets/Scripts/Action Framework/Action Spawners/Action Direct/ActionDirectSystem.cs
@@ -17,18 +17,37 @@ namespace SquareBattle
         {
             var cmd = CommandBuffer.CreateCommandBuffer();
 
+            var buffer = GetBufferFromEntity<PlayingState>(true);
+
             Entities.WithAll<ActionDirect>().ForEach((Entity e, DynamicBuffer<ActionBufferData> actions, in InputEvent input, in ChannelData channel) =>
             {
-                //if(input.value > 0)
-                //{
-                //    var a = cmd.Instantiate(actions[0].action);
-                //    cmd.AddComponent(a, new ActionData()
-                //    {
-                //        owner = e,
-                //        inputEvent = e
-                //    });
-                //    cmd.AddComponent(a, new PlayAction() { });
-                //}
+                bool exist = false;
+                if (buffer.Exists(input.owner))
+                {
+                    var states = buffer[input.owner];
+
+                    for (int i = 0; i < states.Length; i++)
+                    {
+                        if (states[i].channel == channel.channel)
+                        {
+                            exist = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (exist)
+                    return;
+
+                var ac = cmd.Instantiate(actions[0].action);
+                cmd.AddComponent(ac, new OnPlayUpdate() { loop = true });
+                cmd.AddComponent(ac, new ChannelData() { channel = channel.channel });
+                cmd.AddComponent(ac, new ActionData()
+                {
+                    owner = input.owner,
+                    inputEvent = e
+                });
+
             }).Run();
 
             CommandBuffer.AddJobHandleForProducer(Dependency);
